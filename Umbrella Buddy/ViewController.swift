5498b083388b449d1f0ead92477e460b7ef3fa0e
jaredtalbert <jjtalbert@uh.edu>
2019-12-17 02:02:11 -0500

    Initial Commit

---
+++ Umbrella Buddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Umbrella Buddy
+//
+//  Created by Jared Talbert on 12/17/19.
+//  Copyright © 2019 Jared Talbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
